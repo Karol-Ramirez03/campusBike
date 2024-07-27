ee7a74a2bc2ca5a9b55a21de0e89d465dedc97fb
Lauraw100 <158192937+Lauraw100@users.noreply.github.com>
2024-07-27 16:28:12 -0500

    feat :sparkles: EndCaso3&

--- casos de subconsultas/caso 4.sql
+++ casos de subconsultas/caso 4.sql
@@ -1,14 +1,26 @@
 3. El sistema muestra una lista de repuestos ordenados por la cantidad vendida, de menor a
 mayor.
 
-SELECT
+SELECT 
     r.nombre_repuesto,
-    r.descripcion_repuesto,
-    SUM(dv.cantidad) AS cantidad_vendida
-FROM
+    ventas_por_repuesto.cantidad_vendida
+FROM 
     repuesto r
-    JOIN detalle_venta dv ON r.id = dv.id_repuesto
-GROUP BY
-    r.id
-ORDER BY
-    cantidad_vendida ASC;
+JOIN 
+    (SELECT 
+        dv.id_repuesto,
+        SUM(dv.cantidad) AS cantidad_vendida
+     FROM 
+        detalle_venta dv
+     JOIN 
+        bicicleta b ON dv.id_bicicleta = b.id
+     JOIN 
+        modelo_bicicleta mb ON b.id_modelo_bicicleta = mb.id
+     WHERE 
+        mb.id IN (SELECT DISTINCT mb.id FROM modelo_bicicleta mb)
+     GROUP BY 
+        dv.id_repuesto
+    ) ventas_por_repuesto ON r.id = ventas_por_repuesto.id_repuesto
+ORDER BY 
+    ventas_por_repuesto.cantidad_vendida ASC;
+
